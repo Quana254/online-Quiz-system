4b370cda151601913e6fa693acd7602b49e34944
Quana254 <brightonbarevo2000@gmail.com>
2021-11-22 17:26:14 +0300

    the session start of the quiz system page

---
+++ main.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>online quiz system</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header>
+        <div class="container">
+            <p>sample quiz</p>
+        </div>
+    </header>
+    <main>
+        <div class="container">
+            <h2>Test your knowledge</h2>
+            <p>
+                This is a multiple choice quiz to test your knowledge.
+            </p>
+            <ul>
+                <li><strong>Number of questions</strong></li>
+                <li><strong>TYpe:</strong>Multiple choice</li>
+                <li><strong>Estimated time</strong>5 minutes</li>
+            </ul>
+            <a href="question.php?n=1" class="start">start quiz</a>
+        </div>
+    </main>
+</body>
+</html>
