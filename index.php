d1e4bcc725a1a680732076f73c0240c96993e976
Quana254 <brightonbarevo2000@gmail.com>
2021-11-22 17:23:22 +0300

    the home of the system

---
+++ index.php
@@ -0,0 +1,21 @@
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Online-quiz-system</title>
+     <link rel="stylesheet" type="text/css" href="css/style.css">
+ </head>
+ <body>
+     <header>
+         <div class="container">
+             <p>Quizer</p>
+         </div>
+     </header>
+     <main>
+         <a href="main.php" class="button">Take quiz</a>
+         <a href="add.php" class="button">Add quiz</a>
+     </main>
+ </body>
+ </html>
